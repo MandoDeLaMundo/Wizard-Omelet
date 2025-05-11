8f35d79032476832905e90d01fa29ede40feed96
Montiel_Armando <98683036+MandoDeLaMundo@users.noreply.github.com>
2025-05-11 13:11:10 -0400

    ADDED cardinal movement and attached camera to the player.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpForce;
+    [SerializeField] int gravity;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    bool isSprinting;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +23,23 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+            playerVel = Vector3.zero;
+
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
+
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+       // Jump();
+
+        controller.Move(playerVel * Time.deltaTime);
+        playerVel.y = gravity * Time.deltaTime;
+
+    }
+
 }
